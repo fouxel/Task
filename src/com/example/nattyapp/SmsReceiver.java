71329d3e01eed675c96cf4ad82833e710ed10c7f
Jakub Bednarek <j.bednarek94@gmail.com>
2015-10-18 17:08:16 +0200

    Another small modifications
    updated graphics
    updated Activity View
    
    Signed-off-by: Jakub Bednarek <j.bednarek94@gmail.com>

--- src/com/example/nattyapp/SmsReceiver.java
+++ src/com/example/nattyapp/SmsReceiver.java
@@ -10,6 +10,8 @@ import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,6 +19,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -26,16 +30,25 @@ import android.widget.Toast;
 public class SmsReceiver extends BroadcastReceiver {
 
 	private static final String TASK_PREFIX = ".task";
+	private static final String SMS_RECEIVED_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+	private static int notificationIdBase = 0;
+	NotificationCompat.Builder mBuilder;
+	
+	
+	public SmsReceiver() { 
+		super();
+		mBuilder = null;
+	}
 	
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+		if (intent.getAction().equals(SMS_RECEIVED_ACTION)) {
 			for (SmsMessage smsMessage : this.getMessagesFromIntent(intent)) { 
 				String messageBody = smsMessage.getMessageBody();
 				messageBody = messageBody.toLowerCase();
-				if(messageBody.contains(TASK_PREFIX)) { 
-					Toast.makeText(context, "Zadanie dodane!", Toast.LENGTH_LONG).show();
-					messageBody = messageBody.replace(TASK_PREFIX, "");
-					addEventToCalendar(context, messageBody);
+				if(messageBody.contains(TASK_PREFIX)) {
+					long beginTime = getBeginTime(messageBody);
+					Intent resultIntent = addEventToCalendar(context, beginTime);
+					addNotification(context, resultIntent, beginTime);
 				}
 			}
 		}
@@ -55,24 +68,48 @@ public class SmsReceiver extends BroadcastReceiver {
     }
 	
 	@SuppressLint("NewApi")
-	private void addEventToCalendar(Context context, String messageBody) {
+	private Intent addEventToCalendar(Context context, long beginTime) {
 		Calendar cal = Calendar.getInstance();
 		if(Build.VERSION.SDK_INT >= 14) { 
-			Intent intent2 = new Intent(Intent.ACTION_INSERT).setData(Events.CONTENT_URI)
+			Intent intent = new Intent(Intent.ACTION_INSERT).setData(Events.CONTENT_URI)
 					.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, cal.getTimeInMillis())
-					.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, getBeginTime(messageBody))
-					.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, getBeginTime(messageBody) + 60*1000)
+					.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime)
+					.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, beginTime + 60*1000)
 					.putExtra(Events.TITLE, "Spotkanie")
 					.putExtra(Events.DESCRIPTION, "Spotkanie")
 					.putExtra(Events.EVENT_LOCATION, "Moczydla")
 					.putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
 					.putExtra(CalendarContract.Reminders.MINUTES, 5);
-		    intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startActivity(intent2);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			return intent;
 		}
+		return null;
+	}
+	
+	private void addNotification(Context context, Intent resultIntent, long beginTime) { 
+		Date date = new Date(beginTime);
+	    mBuilder = new NotificationCompat.Builder(context)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setAutoCancel(true)
+				.setContentTitle("Nowe zadanie")
+				.setContentText("Data: " + date.toLocaleString());
+	    
+		
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+		stackBuilder.addParentStack(MainActivity.class);
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent = 
+				stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager = 
+			(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+	
+		mNotificationManager.notify(notificationIdBase, mBuilder.build());
+		notificationIdBase++;
 	}
 	
 	private long getBeginTime(String input) { 
+		input = input.replace(TASK_PREFIX, "");
 		List<Date> dateList = new ArrayList<Date>();
 
 		Parser parser = new Parser();
@@ -84,7 +121,7 @@ public class SmsReceiver extends BroadcastReceiver {
 			String matchingValue = group.getText();
 			String syntaxTree = group.getSyntaxTree().toStringTree();
 			Map parseMap = group.getParseLocations();
-			boolean isRecurreing = group.isRecurring();
+			boolean isRecurring = group.isRecurring();
 			Date recursUntil = group.getRecursUntil();
 
 			/* if any Dates are present in current group then add them to dateList */
